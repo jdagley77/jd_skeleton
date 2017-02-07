71fbd80da0574f14902b5c3a1c57b333b6c72a64
Jack Dagley <jack.dagley77@gmail.com>
2017-02-06 18:23:00 -0800

    Add user controller and user views

---
+++ app/controllers/user.rb
@@ -0,0 +1,17 @@
+get '/users/new' do
+  erb :"users/new"
+end
+
+post '/users' do
+  @user = User.new(params[:user])
+    if @user.save
+      redirect '/'
+    else
+      erb :"users/new"
+    end
+end
+
+get '/users/:id' do
+  @user = User.find_by(id: params[:id])
+  erb :"users/show"
+end
